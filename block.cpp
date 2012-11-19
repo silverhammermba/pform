5456a30d0eee1ee3ae4549c4d78c7c639599c971
Max <silverhammermba@gmail.com>
2012-11-18 22:04:43 -0500

    Add Block (as sub of StaticEntity)

---
+++ block.cpp
@@ -0,0 +1,5 @@
+#include "pform.hpp"
+
+Block::Block() : StaticEntity(true)
+{
+}
--- pform.hpp
+++ pform.hpp
@@ -2,7 +2,13 @@
 #define PFORM_H_
 
 #include <vector>
+
+#include <SFML/Graphics.hpp>
+
+#define PPB 8 // pixels per block
+
 #include "entity.hpp"
+#include "block.hpp"
 #include "level.hpp"
 
 #endif
