b1cfca48c1fee57a318a8e6eed05ca1ef00c275e
Max <silverhammermba@gmail.com>
2012-11-28 21:32:46 -0500

    Use arrays for all vector-like attributes.
    Remove collision code to start anew!!!

--- test.rb
+++ test.rb
@@ -19,6 +19,16 @@ class Numeric
 		end
 		return 0
 	end
+
+	def clamp lb, ub
+		if self < lb
+			lb
+		elsif self > ub
+			ub
+		else
+			self
+		end
+	end
 end
 
 def reduce y, x
@@ -31,157 +41,79 @@ def reduce y, x
 end
 
 class Player
-	def initialize level, texture, x, y
+	def initialize level, texture, pos
 		@level = level
-		@x = x * $block_size
-		@y = y * $block_size
-		find_relevant_region
 		@sprite = Sprite.new(texture)
-		@sprite.set_position(@x.floor, @y.floor)
+
+		# position
+		@pos = pos.map { |coord| coord * $block_size }
+		# distance to move
+		@diff = [0, 0]
+		# current velocity
+		@vel = [0, 0]
+		# acceleration from movement and gravity
+		@acc = [300, 800]
+		# break speed when idle
+		@break = @acc[0] * 2
+		# terminal velocities
+		@trm = [75, 100]
 		# x direction from keyboard
 		@dir = 0
-		@dx = 0
-		@dy = 0
-		# acceleration when key pressed
-		@accel = 300
-		# break speed when idle
-		@break = @accel * 2
-		# top speed
-		@max = 75
-		# acceleration from gravity
-		@gravity = 800
 		# if the player is on the ground
 		@standing = false
-		# terminal velocity
-		@tv = 100
-		# current velocity
-		@vx = 0
-		@vy = 0
+
+		find_relevant_region
+		@sprite.set_position(*@pos.map(&:floor))
 	end
 
 	attr_accessor :dir
-	attr_reader :sprite, :speed, :minx, :miny, :maxx, :maxy
+	attr_reader :sprite, :limit
 
 	def jump
 		if @standing
-			@x = @x.round # to allow jumping through narrow gaps
-			@vy = -250
+			# align pixels to allow jumping through narrow gaps
+			@pos[0] = @pos[0].round
+			@vel[1] = -250
 			@standing = false
 		end
 	end
 
 	# find blocks overlapped by the player
 	def find_relevant_region
-		@minx = (@x / $block_size).floor
-		@maxx = (@x / $block_size).ceil
-		@miny = (@y / $block_size).floor
-		@maxy = (@y / $block_size).ceil
+		# this weird array lets us access the min coords as @limit[-1] and max as @limit[1]
+		@limit = [nil, @pos.map { |coord| (coord / $block_size).ceil }, @pos.map { |coord| (coord / $block_size).floor }]
 	end
 
 	# move the player
 	def step seconds
-		if @vx != 0 and @dir * @vx <= 0
-			@vx = reduce(@vx, @break * seconds)
-		else # @vx == 0 or @dir * @vx > 0
-			@vx += @dir * @accel * seconds
-			if @vx > @max
-				@vx = @max
-			elsif @vx < -@max
-				@vx = -@max
-			end
+		# if moving and no key pressed, break
+		if @vel[0] != 0 and @dir * @vel[0] <= 0
+			@vel[0] = reduce(@vel[0], @break * seconds)
+		else # @vel[0] == 0 or @dir * @vel[0] > 0
+			@vel[0] += @dir * @acc[0] * seconds
 		end
+		# fall faster if not standing
 		if not @standing
-			@vy += @gravity * seconds
+			@vel[1] += @acc[1] * seconds
 		end
-		@dx = @vx * seconds
-		@dy = @vy * seconds
-		if @dx != 0 or @dy != 0
-			resolve_movement
+		# clamp velocity
+		@vel.map!.with_index { |v, i| v.clamp(-@trm[i], @trm[i]) }
+
+		@diff = @vel.map { |v| v * seconds }
+		if @diff.any? { |d| d != 0 }
+			#resolve_movement
 			find_relevant_region
-			@sprite.set_position(@x, @y)
+			@sprite.set_position(*@pos)
 		end
 	end
 
 	def stand!
 		@standing = true
-		@vy = 0
+		@vel[1] = 0
 	end
 
 	def fall!
 		@standing = false
-	end
-
-	def align_x!
-		@x = (@x / $block_size).round * $block_size
-	end
-
-	def x_aligned?
-		@x % $block_size == 0
-	end
-
-	def align_y!
-		@y = (@y / $block_size).round * $block_size
-	end
-
-	def y_aligned?
-		@y % $block_size == 0
-	end
-
-	def resolve_movement
-		if @dx != 0 and @dy == 0
-			minmax = (@dx > 0 ? @maxx : @minx)
-			mod = (@dx > 0 ? 1 : -1)
-			if x_aligned?
-				# fall if shouldn't be standing
-				if @standing and @level[@maxy + 1][@minx].nil?
-					fall!
-					# TODO fall a little?
-					return
-				else
-					unless @level[@miny][minmax + mod] or @level[@maxy][minmax + mod]
-						@x += @dx
-						return
-					end
-				end
-			else
-				if (@x + @dx) * @dx > minmax * $block_size * @dx
-					x = @x
-					@x = minmax * $block_size
-					@dx -= @x - x
-					resolve_movement
-				else
-					@x += @dx
-				end
-				return
-			end
-		elsif @dx == 0 and @dy != 0
-			minmax = (@dy > 0 ? @maxy : @miny)
-			mod = (@dy > 0 ? 1 : -1)
-			if y_aligned?
-				# fall if shouldn't be standing
-				if not @standing and @level[@maxy + 1][@minx]
-					stand!
-					return
-				else
-					unless @level[minmax + mod][@minx] or @level[minmax + mod][@maxx]
-						@y += @dy
-						return
-					end
-				end
-			else
-				if (@y + @dy) * @dy > minmax * $block_size * @dy
-					y = @y
-					@y = minmax * $block_size
-					@dy -= @y - y
-					resolve_movement
-				else
-					@y += @dy
-				end
-				return
-			end
-		else # @dx != 0 and @dy != 0
-			# TODO FUCKING SHIT
-		end
 	end
 end
 
@@ -216,32 +148,23 @@ fps_text.set_color(Color::Black)
 
 # load textures
 dude = Texture.new
-$brick = Texture.new
+brick = Texture.new
 dude.load_from_file("char.png")
-$brick.load_from_file("block.png")
+brick.load_from_file("block.png")
 
 # create level
-level = Array.new(bh) { |i| Array.new(bw) { |j| nil } }
+level = Array.new(bw) { |i| Array.new(bh) { |j| nil } }
 
-def block lvl, x, y
-	lvl[y][x] = Block.new($brick, x, y)
-end
-
-block(level, 0, 1)
+blocks = [
+	[0, 1], [0, 5], [1, 5], [1, 3], [1, 6], [0, 6], [7, 5], [7, 6], [9, 5], [9, 6]
+]
 for i in (0...bw)
-	block(level, i, bh - 1)
+	blocks << [i, bh - 1]
 end
-block(level, 0, 5)
-block(level, 1, 5)
-block(level, 1, 3)
-block(level, 1, 6)
-block(level, 0, 6)
-block(level, 7, 5)
-block(level, 7, 6)
-block(level, 9, 5)
-block(level, 9, 6)
 
-player = Player.new(level, dude, 7, 4)
+blocks.each { |x, y| level[x][y] = Block.new(brick, x, y) }
+
+player = Player.new(level, dude, [7, 4])
 
 green = Color.new(0, 255, 0, 127)
 debug = RectangleShape.new([$block_size, $block_size])
@@ -295,8 +218,8 @@ while window.open?
 	level.each { |row| row.each { |block| window.draw(block.sprite) if block } }
 	window.draw(player.sprite)
 
-	for i in ((player.miny)..(player.maxy))
-		for j in ((player.minx)..(player.maxx))
+	((player.limit[-1][1])..(player.limit[1][1])).each do |i|
+		((player.limit[-1][0])..(player.limit[1][0])).each do |j|
 			debug.set_position(j * $block_size, i * $block_size)
 			window.draw(debug)
 		end
